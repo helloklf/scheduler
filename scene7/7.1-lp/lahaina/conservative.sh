2de572c759c031e3d9fdc2fc8bfe2e0eea0a4b9b
kuanglinfei <helloklf@outlook.com>
2024-02-15 15:55:08 +0800

    创建7.1配置目录

---
+++ scene7/7.1-lp/lahaina/conservative.sh
@@ -0,0 +1,17 @@
+action=$1
+task=$2
+
+cfg_dir=$(cd $(dirname $0); pwd)
+
+init () {
+  if [[ -f "$cfg_dir/powercfg-base.sh" ]]; then
+    source "$cfg_dir/powercfg-base.sh"
+  elif [[ -f '/data/powercfg-base.sh' ]]; then
+    source /data/powercfg-base.sh
+  fi
+}
+
+if [[ "$action" == "init" ]]; then
+  init
+  exit 0
+fi
