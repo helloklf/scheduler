f8728e35306f1f6342cf31438a14dafe88a6612b
kuanglinfei <helloklf@outlook.com>
2025-11-13 15:20:29 +0800

    适配8Elite GEN5

--- scene9/9.0/lp/canoe/powercfg.sh
+++ scene9/9.0/lp/canoe/powercfg.sh
@@ -127,7 +127,6 @@ if [[ -d  /proc/game_opt ]]; then
   # hide_value /proc/game_opt/disable_cpufreq_limit 1
   set_value '1000' /proc/oplus-votable/GAUGE_UPDATE/force_val
   set_value '1' /proc/oplus-votable/GAUGE_UPDATE/force_active
-  stop thermal-engine
   stop oplusHmbirdBpfManager
   echo -1 > /proc/oplus_hmbird/manager_pid
 fi
